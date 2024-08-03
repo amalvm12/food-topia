3361eecd39d40c2a809b26833c928583df2db829
AMAL V MUKUNDAN <117355540+amalvm12@users.noreply.github.com>
2024-08-03 15:57:40 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,9 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+export const url = 'https://food-topia-backend.onrender.com'
+
+
 export const assets ={
     logo,
     add_icon,
@@ -14,4 +17,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-topia-backend.onrender.com'
+
