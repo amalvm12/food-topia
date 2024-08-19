98587ff7312829874ba68d6c72f14c49d9f34687
amalvm12 <amalvm@73gamil.com>
2024-08-20 00:59:03 +0530

    modified

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -28,7 +28,7 @@ const placeOrder = async (req,res)=>{
                 product_data:{
                     name:item.name
                 },
-                unit_amount:item.price*100*80
+                unit_amount:item.price
             },
             quantity:item.quantity
 
@@ -39,7 +39,7 @@ const placeOrder = async (req,res)=>{
                 product_data:{
                     name:"Delivery charges"
                 },
-                unit_amount:2*100*80
+                unit_amount:20
             },
             quantity:1
 
